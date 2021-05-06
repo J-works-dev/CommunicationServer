771a46bd4b645ce771281c534ac64dda376e4999
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-05-06 11:04:13 +0800

    Initial Commit

---
+++ CommunicationServer/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace CommunicationServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPw = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelPw = new System.Windows.Forms.Label();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxId.Location = new System.Drawing.Point(25, 130);
+            this.textBoxId.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(152, 30);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Log In:";
+            // 
+            // textBoxPw
+            // 
+            this.textBoxPw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPw.Location = new System.Drawing.Point(185, 130);
+            this.textBoxPw.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPw.Name = "textBoxPw";
+            this.textBoxPw.Size = new System.Drawing.Size(154, 30);
+            this.textBoxPw.TabIndex = 2;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.Location = new System.Drawing.Point(22, 108);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(21, 18);
+            this.labelId.TabIndex = 3;
+            this.labelId.Text = "ID";
+            // 
+            // labelPw
+            // 
+            this.labelPw.AutoSize = true;
+            this.labelPw.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPw.Location = new System.Drawing.Point(182, 108);
+            this.labelPw.Name = "labelPw";
+            this.labelPw.Size = new System.Drawing.Size(67, 18);
+            this.labelPw.TabIndex = 4;
+            this.labelPw.Text = "Password";
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSend.Location = new System.Drawing.Point(25, 352);
+            this.textBoxSend.Multiline = true;
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(356, 70);
+            this.textBoxSend.TabIndex = 5;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(387, 352);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(88, 70);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(346, 130);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(129, 30);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start Server";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusText});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 445);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(497, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusText
+            // 
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(0, 16);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Pipe Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 26);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Messages:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 197);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(450, 142);
+            this.textBox2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 467);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.labelPw);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.textBoxPw);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxId);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPw;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelPw;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CommunicationServer/User.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationServer
+{
+    class User
+    {
+        private string id;
+        private string password;
+        private const int SALT_SIZE = 16;
+        private const int HASH_SIZE = 20;
+
+        public User(string id, string password)
+        {
+            this.id = id;
+
+            // Create Salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SALT_SIZE]);
+
+            // Creat Hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            var hash = pbkdf2.GetBytes(HASH_SIZE);
+
+            // Combine salt and hash
+            var hashBytes = new byte[SALT_SIZE + HASH_SIZE];
+            Array.Copy(salt, 0, hashBytes, 0, SALT_SIZE);
+            Array.Copy(hash, 0, hashBytes, SALT_SIZE, HASH_SIZE);
+
+            // Convert to base64
+            var base64Hash = Convert.ToBase64String(hashBytes);
+
+            // Save formatted hash with extra information in password
+            this.password = string.Format("$MYHASH$V1${0}", base64Hash);
+        }
+
+        public bool Verify(string id, string pw)
+        {
+            // var splittedHash = password.Replace("$MYHASH$V1$", "").Split('$');
+            // var base64Hash = splittedHash[1];
+            var base64Hash = password.Replace("$MYHASH$V1$", "");
+
+            // Get hash bytes
+            var hashBytes = Convert.FromBase64String(base64Hash);
+
+            // Get Salt
+            var salt = new byte[SALT_SIZE];
+            Array.Copy(hashBytes, 0, salt, 0, SALT_SIZE);
+
+            // Creat hash with given salt
+            var pbkdf2 = new Rfc2898DeriveBytes(pw, salt);
+            byte[] hash = pbkdf2.GetBytes(HASH_SIZE);
+
+            for (int i = 0; i < HASH_SIZE; i++)
+            {
+                if (hashBytes[i + SALT_SIZE] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
